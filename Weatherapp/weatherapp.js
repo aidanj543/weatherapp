61ef0801f4c36ce4ed0dc62f959a430363e786d4
aidanj543 <152727247+aidanj543@users.noreply.github.com>
2024-03-03 14:10:24 -0600

    Clock added

---
+++ Weatherapp/weatherapp.js
@@ -0,0 +1,23 @@
+function updateClock(){
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+
+    //this will add zeros to single digit numbers
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes <10) ? "0" + minutes: minutes;
+    seconds = (seconds <10) ? "0" + seconds: seconds;
+
+    //this line of code is basically the format of the clock
+    let timeString = hours + ":" + minutes + ":" + seconds;
+
+    //this will update the current clock div I put on the html code
+    document.getElementById("clock").textContent = timeString;
+
+    //this will call this function to update every second
+    setTimeout(updateClock, 1000);
+}
+
+//this will call the updateClock function for the first time
+updateClock();
